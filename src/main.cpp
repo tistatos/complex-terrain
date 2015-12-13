ded44821f686407563dac30a58723b0c6a441f85
tistatos <tistatos@gmail.com>
2015-12-13 23:32:02 +0100

    fixed glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,60 @@
 * @brief [Description Goes Here]
 */
 
-#include "../include/engine.h"
+#include <iostream>
+
+#include "engine.h"
 
 int main(int argc, char* argv[])
 {
   const GLFWvidmode* vidmode;
   GLFWwindow* window;
+  GLFWmonitor* monitor;
   char title[64] = "Complex Terrain";
 
   double dt;
   double fps;
 
   if(!glfwInit()) return -1;
+
+  monitor = glfwGetPrimaryMonitor();
+  vidmode = glfwGetVideoMode(monitor);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  window = glfwCreateWindow(vidmode->width/2, vidmode->height/2, title, NULL, NULL);
+
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(0);
+
+  glewInit();
+
+
+  while (!glfwWindowShouldClose(window))
+  {
+    // Set the clear color and depth, and clear the buffers for drawing
+    glClearColor(0.3f, 0.3f, 0.3f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE))
+    {
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
 }
 
